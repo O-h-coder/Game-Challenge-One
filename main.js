d491773c3e93bed6915e724abee796946d4984b5
O-h-Coder <omar.hanfy.1287@gmail.com>
2026-05-07 00:05:31 +0200

    Add Gallery Section And Finesh File main.js and Style This Section and Add Images.

---
+++ main.js
@@ -0,0 +1,16 @@
+let current = 1;
+function changeSlide(el, title, desc) {
+  const bg1 = document.querySelector(".bg1");
+  const bg2 = document.querySelector(".bg2");
+  let nextBg = current === 1 ? bg2 : bg1;
+  let currentBg = current === 1 ? bg1 : bg2;
+  nextBg.style.backgroundImage = `url(${el.src})`;
+  nextBg.style.opacity = "1";
+  currentBg.style.opacity = "0";
+  current = current === 1 ? 2 : 1;
+  document.getElementById("title").innerText = title;
+  document.getElementById("dosc").innerText = desc;
+  let imgs = document.querySelectorAll(".imagesimg");
+  imgs.forEach((img) => img.classList.remove("active"));
+  element.classList.add("active");
+}
